59afd17191e28ab2a8f2fb8b245347ba29542ba8
Akashi Park <61649527+akashipark@users.noreply.github.com>
2021-02-02 22:39:02 +0800

    个性化文件存储

---
+++ websign.php
@@ -0,0 +1,55 @@
+<!--
+   项目名称：明石留言板
+   版本：V1.2
+   时间：2021-1-29
+   
+   Copyright©2021 | Akashi Soft
+   遵循 CC-BY-NC-SA 版权协议
+   
+   websign.php
+-->
+
+<?php require_once( '../../header.php'); ?>
+
+
+<?php
+
+if( $_SERVER['HTTP_REFERER'] == "" )
+{
+
+exit('<br><br><h1><center>非法操作，请勿直接访问本页面。</center></h1><br><br>');
+}		
+
+?>
+
+<?php 
+	
+   
+	
+ 
+	// 首先判断Cookie是否有记住了用户信息
+	if (isset($_COOKIE['username'])) {
+		# 若记住了用户信息,则直接传给Session
+		$_SESSION['islogin'] = 1;
+	}
+	if (isset($_SESSION['islogin'])) {
+		
+	} else {
+		// 若没有登录
+		exit("<br><br><h1><center><div class=\"mdui-typo\">您还没有登录，请<a href=\"./index.php\">登录</a></div></h1></center><br><br>");
+	}
+ ?>
+
+
+<?php 
+	
+  $loadwebsigndat = fopen("websign.dat", "w");
+  $modifywebsigndat = $_POST['changewebsign'];
+  fwrite($loadwebsigndat, $modifywebsigndat);
+  fclose($loadwebsigndat);
+
+  $url = "../../admin.php";  
+               echo "<script language='javascript' type='text/javascript'>window.location.href='$url'</script>";  
+
+?>
+ 
